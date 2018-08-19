76d4640766c5e9618b467b74324a4cf9a5a036da
Richo Healey <richo@psych0tik.net>
2018-08-18 20:44:28 -0700

    Move the cli parser out into it's own fn

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,29 @@
 extern crate serde_derive;
 
 extern crate clap;
-use clap::App;
+use clap::{App,SubCommand,Arg};
 
 mod version;
 mod config;
 
-fn main() {
+fn cli_opts<'a, 'b>() -> App<'a, 'b> {
     App::new("archiver")
         .version(version::VERSION)
         .about("Footage archiver")
         .author("richö butts")
-        .get_matches();
+        .subcommand(SubCommand::with_name("daemon")
+                    .version(version::VERSION)
+                    .author("richö butts")
+                    .about("Runs archiver in persistent mode"))
+        .subcommand(SubCommand::with_name("run")
+                    .about("Runs archiver in persistent mode")
+                    .version(version::VERSION)
+                    .author("richö butts")
+                    .arg(Arg::with_name("device")
+                         .short("d")
+                         .help("Upload only from device")))
+}
+
+fn main() {
+    cli_opts().get_matches();
 }
