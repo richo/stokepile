eabc69597c93199a5e91e5278781f72ce7bf79f5
Richo Healey <richo@psych0tik.net>
2018-08-18 22:25:28 -0700

    Error handling, reworked config

--- src/config.rs
+++ src/config.rs
@@ -1,79 +1,258 @@
+extern crate toml;
+extern crate failure;
+
+use std::fs::File;
+use std::io::Read;
+
+use failure::Error;
+
+
 #[allow(non_camel_case_types)]
 #[derive(Deserialize,Debug,Eq,PartialEq)]
-enum StorageBackend {
+pub enum StorageBackend {
     dropbox,
 }
 
 #[derive(Deserialize,Debug,Eq,PartialEq)]
-struct Config {
+pub struct Config {
     archiver: ArchiverConfig,
-    dropbox: Option<DropboxConfig>,
+    dropbox: DropboxConfig,
     // vimeo: Option<VimeoConfig>,
     // youtube: Option<YoutubeConfig>,
-    flysight: Vec<FlysightConfig>,
-    gopro: Vec<GoproConfig>,
+    flysight: Option<Vec<FlysightConfig>>,
+    gopro: Option<Vec<GoproConfig>>,
     // gswoop: Option<GswoopConfig>,
     // sendgrid: Option<SendgridConfig>,
     // pushover: Option<PushoverConfig>,
 }
 
+lazy_static! {
+    static ref EMPTY_GOPROS: Vec<GoproConfig> = vec![];
+    static ref EMPTY_FLYSIGHTS: Vec<FlysightConfig> = vec![];
+}
+
 #[derive(Deserialize,Debug,Eq,PartialEq)]
-struct ArchiverConfig {
+pub struct ArchiverConfig {
     storage_backend: StorageBackend,
 }
 
 #[derive(Deserialize,Debug,Eq,PartialEq)]
-struct DropboxConfig {
+pub struct DropboxConfig {
     token: String,
 }
 
 #[derive(Deserialize,Debug,Eq,PartialEq)]
-struct FlysightConfig {
+pub struct FlysightConfig {
     name: String,
-    mountpoint: Option<String>,
-    uuid: Option<String>,
+    mountpoint: String,
 }
 
 #[derive(Deserialize,Debug,Eq,PartialEq)]
-struct GoproConfig {
+pub struct GoproConfig {
     name: String,
-    mountpoint: Option<String>,
-    uuid: Option<String>,
+    mountpoint: String,
+}
+
+impl Config {
+    pub fn from_file(path: &str) -> Result<Config, Error> {
+        let mut fh = File::open(path)?;
+        let mut contents = String::new();
+        fh.read_to_string(&mut contents)?;
+
+        Config::from_str(&contents)
+    }
+
+    pub fn from_str(body: &str) -> Result<Config, Error> {
+        match toml::from_str(body) {
+            Ok(config) => Ok(config),
+            Err(e) => Err(format_err!("Couldn't parse config: {}", e)),
+        }
+    }
+
+    // Do we eventually want to make a camera/gopro distinction?
+    pub fn gopros(&self) -> &Vec<GoproConfig> {
+        match self.gopro {
+            None => &EMPTY_GOPROS,
+            Some(ref v) => v,
+        }
+    }
+
+    pub fn flysights(&self) -> &Vec<FlysightConfig> {
+        match self.flysight {
+            None => &EMPTY_FLYSIGHTS,
+            Some(ref v) => v,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    extern crate toml;
-
     use super::*;
-    use std::fs::File;
-    use std::io::Read;
 
     #[test]
     fn test_example_config_parses() {
-        let mut fh = File::open("archiver.toml.example").unwrap();
-        let mut contents = String::new();
-        fh.read_to_string(&mut contents).unwrap();
-
-        let config: Config = toml::from_str(&contents).unwrap();
+        let config = Config::from_file("archiver.toml.example").unwrap();
 
         assert_eq!(config.archiver.storage_backend, StorageBackend::dropbox);
 
         assert_eq!(config.dropbox,
-                   Some(DropboxConfig{ token: "DROPBOX_TOKEN_GOES_HERE".into() }));
+                   DropboxConfig{ token: "DROPBOX_TOKEN_GOES_HERE".into() });
 
         assert_eq!(config.flysight,
-                   vec![FlysightConfig {
+                   Some(vec![FlysightConfig {
                             name: "data".into(),
-                            mountpoint: Some("/mnt/archiver/flysight".into()),
-                            uuid: None,
-                   }]);
+                            mountpoint: "/mnt/archiver/flysight".into(),
+                   }]));
 
         assert_eq!(config.gopro,
-                   vec![GoproConfig {
+                   Some(vec![GoproConfig {
                             name: "video".into(),
-                            mountpoint: Some("/mnt/archiver/gopro".into()),
-                            uuid: None,
-                   }]);
+                            mountpoint: "/mnt/archiver/gopro".into(),
+                   }]));
+    }
+
+    #[test]
+    fn test_invalid_backend() {
+        let error = Config::from_str(r#"
+[archiver]
+storage_backend="butts"
+"#).unwrap_err();
+        assert!(format!("{}", error)
+                .contains("unknown variant `butts`, expected `dropbox` for key `archiver.storage_backend`"))
+    }
+
+    #[test]
+    fn test_no_backend() {
+        let error = Config::from_str(r#"
+[archiver]
+"#).unwrap_err();
+        assert!(format!("{}", error)
+                .contains("missing field `storage_backend` for key `archiver`"))
+    }
+
+    #[test]
+    fn test_no_dropbox() {
+        let error = Config::from_str(r#"
+[archiver]
+storage_backend="dropbox"
+"#).unwrap_err();
+        assert!(format!("{}", error)
+                .contains("missing field `dropbox`"))
+    }
+
+    #[test]
+    fn test_no_peripherals() {
+        let config = Config::from_str(r#"
+[archiver]
+storage_backend="dropbox"
+[dropbox]
+token="DROPBOX_TOKEN_GOES_HERE"
+"#).unwrap();
+        assert_no_gopros(&config);
+        assert_no_flysights(&config);
+    }
+
+    fn assert_no_gopros(cfg: &Config) {
+        assert_eq!(cfg.gopro, None);
+        assert_eq!(cfg.gopros(), &vec![]);
+    }
+
+    fn assert_no_flysights(cfg: &Config) {
+        assert_eq!(cfg.flysight, None);
+        assert_eq!(cfg.flysights(), &vec![]);
+    }
+
+    fn assert_gopros(cfg: &Config) {
+        assert_eq!(cfg.gopros(),
+        &vec![GoproConfig {
+                name: "front".into(),
+                mountpoint: "/mnt/archiver/front".into(),
+            },
+            GoproConfig {
+                name: "back".into(),
+                mountpoint: "/mnt/archiver/back".into(),
+            }
+        ])
+    }
+
+    fn assert_flysights(cfg: &Config) {
+        assert_eq!(cfg.flysights(),
+        &vec![FlysightConfig {
+                name: "training".into(),
+                mountpoint: "/mnt/archiver/training".into(),
+            },
+            FlysightConfig {
+                name: "comp".into(),
+                mountpoint: "/mnt/archiver/comp".into(),
+            }
+        ])
+    }
+
+    #[test]
+    fn test_gopros() {
+        let config = Config::from_str(r#"
+[archiver]
+storage_backend="dropbox"
+[dropbox]
+token="DROPBOX_TOKEN_GOES_HERE"
+
+[[gopro]]
+name = "front"
+mountpoint="/mnt/archiver/front"
+
+[[gopro]]
+name = "back"
+mountpoint="/mnt/archiver/back"
+"#).unwrap();
+        assert_gopros(&config);
+        assert_no_flysights(&config);
+    }
+
+    #[test]
+    fn test_flysights() {
+        let config = Config::from_str(r#"
+[archiver]
+storage_backend="dropbox"
+[dropbox]
+token="DROPBOX_TOKEN_GOES_HERE"
+
+[[flysight]]
+name = "training"
+mountpoint="/mnt/archiver/training"
+
+[[flysight]]
+name = "comp"
+mountpoint="/mnt/archiver/comp"
+"#).unwrap();
+        assert_flysights(&config);
+        assert_no_gopros(&config);
+    }
+
+    #[test]
+    fn test_gopros_and_flysights() {
+        let config = Config::from_str(r#"
+[archiver]
+storage_backend="dropbox"
+[dropbox]
+token="DROPBOX_TOKEN_GOES_HERE"
+
+[[gopro]]
+name = "front"
+mountpoint="/mnt/archiver/front"
+
+[[gopro]]
+name = "back"
+mountpoint="/mnt/archiver/back"
+
+[[flysight]]
+name = "training"
+mountpoint="/mnt/archiver/training"
+
+[[flysight]]
+name = "comp"
+mountpoint="/mnt/archiver/comp"
+"#).unwrap();
+        assert_gopros(&config);
+        assert_flysights(&config);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate failure;
+#[macro_use] extern crate lazy_static;
 
 extern crate clap;
 use clap::{App,SubCommand,Arg};
@@ -12,6 +13,9 @@ fn cli_opts<'a, 'b>() -> App<'a, 'b> {
         .version(version::VERSION)
         .about("Footage archiver")
         .author("richö butts")
+        .arg(Arg::with_name("config")
+             .takes_value(true)
+             .help("Path to configuration file"))
         .subcommand(SubCommand::with_name("daemon")
                     .version(version::VERSION)
                     .author("richö butts")
@@ -22,9 +26,33 @@ fn cli_opts<'a, 'b>() -> App<'a, 'b> {
                     .author("richö butts")
                     .arg(Arg::with_name("device")
                          .short("d")
+                         .takes_value(true)
+                         .multiple(true)
                          .help("Upload only from device")))
 }
 
+// fn load_config(path: &str) -> config::Config {
+//     match config::Config::from_file(path) {
+//         Ok(cfg) => cfg,
+//         _ => unimplemented!(),
+//     }
+// }
+
 fn main() {
-    cli_opts().get_matches();
+    let matches = cli_opts().get_matches();
+
+    // Loading config here lets us bail at a convenient time before we get in the weeds
+
+    // let config = load_config(matches.value_of("config").unwrap_or("archiver.toml"));
+
+    match matches.subcommand() {
+        ("daemon", Some(subm))  => {
+            unimplemented!();
+        },
+        ("run", Some(subm)) => {
+
+
+        },
+        _ => {unreachable!()}, // Either no subcommand or one not tested for...
+    }
 }
