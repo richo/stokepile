460d2a88fbd32cd76c3fe083fe0e8325f49d0067
Richo Healey <richo@psych0tik.net>
2019-02-04 15:44:31 -0800

    Add more helpers to the cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,20 @@
+use crate::{AUTHOR, VERSION};
+
+use clap::{App, Arg};
+
+/// Create the base set of clap options common to all cli commands
+pub fn base_opts<'a, 'b>() -> App<'a, 'b> {
+    App::new("archiver")
+        .version(VERSION)
+        .author(AUTHOR)
+        .arg(
+            Arg::with_name("config")
+                .long("config")
+                .takes_value(true)
+                .help("Path to configuration file"),
+        )
+}
+
 /// Setup logging for archiver. This sets the log level to INFO if unset and configures the logging
 /// facade favoured by archiver's clis.
 pub fn init_logging() {
@@ -22,6 +39,9 @@ pub fn init_logging() {
 ///     });
 /// }
 /// ```
+///
+/// Even in our post Termination world, this is valuable in order to assert that logging is setup,
+/// and in future that we have a panic handler.
 pub fn run(main: fn() -> Result<(), ::failure::Error>) {
     init_logging();
     if let Err(e) = main() {
