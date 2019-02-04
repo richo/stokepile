19f02296b56230294c11dc4aa79de687e3e3154a
Richo Healey <richo@psych0tik.net>
2019-02-04 14:43:33 -0800

    Started hacking on a thread-per-device idea

--- src/bin/test-hotplug.rs
+++ src/bin/test-hotplug.rs
@@ -11,11 +11,9 @@ use std::sync::{Mutex, Arc};
 use std::thread;
 use std::time;
 
-use archiver::config;
+use archiver::config::{self, DeviceConfig};
 use archiver::ctx::Ctx;
 use archiver::device;
-
-type DeviceState<'a> = HashMap<device::Device<'a>, Arc<Mutex<AttachedDeviceState>>>;
 
 /// A device that either is attached, or previously has been, to this uploader session.
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -28,77 +26,13 @@ enum AttachedDeviceState {
     Complete,
     /// A device that was disconnected from this session.
     Disconnected,
-    /// A device we're not sure about, this was its last state.
-    Indeterminate(Box<AttachedDeviceState>),
 }
 
-fn poll_devices(state: &mut DeviceState) -> Result<(), Error> {
-    // READ ME AND DISPAIR
-    // attached_devices() creates a new libusb::connection for each device it finds when
-    // translating them into Device objects.
-    //
-    // The new ones are Drop'd when they're not inserted into the Map so *hopefully* this results
-    // in one persistent connection and at most one short lived one at a time, but realistically
-    // this probably means the API is defective.
-    info!("Attached devices:");
-    // First, we go through and set all states to Indeterminate.
-    //
-    // If another thread preempts this by setting its state to Finished, even though the device is
-    // gone, that's ok. The next loop through this method will correct set it to Disconnected.
-    for state in state.values() {
-        let mut inner = state.lock().expect("Setting indeterminate lock");
-        let new_state = AttachedDeviceState::Indeterminate(Box::new(inner.clone()));
-        *inner = new_state;
-    }
-
-    // Look through all the attached devices, setting their state to Connected if they're new, or
-    // unwrapping Indeterminate if that's what we find.
-    for device in device::attached_devices(&ctx)? {
-        info!("  {:?}", device);
-        state.entry(device)
-            .and_modify(|v| {
-                let mut inner = v.lock().expect("Unwrapping indeterminate lock");
-                if let AttachedDeviceState::Indeterminate(inner_state) = inner.deref() {
-                    *inner= *inner_state.clone();
-                }
-            })
-            .or_insert_with(|| Arc::new(Mutex::new(AttachedDeviceState::Connected)));
-    }
-
-    // Finally, we look through them all, and set anything that's Indeterminate to Disconnected.
-    for state in state.values() {
-        let mut inner = state.lock().expect("Disconnected lock");
-        if let AttachedDeviceState::Indeterminate(_) = inner.deref() {
-            *inner = AttachedDeviceState::Disconnected;
-        }
-    }
-
-    Ok(())
+fn dispatch_gopro_watcher(config: &config::GoproConfig) {
+    info!("Dispatching a thread to monitor {:?}", config);
 }
 
-fn spawn_worker_threads(state: &mut DeviceState, work: fn(&device::Device)) {
-    for (device, state) in state.iter_mut() {
-        let mut inner = state.lock().expect("Worker thread lock");
-        if let AttachedDeviceState::Connected = inner.deref() {
-            info!("Inspecting {:?}", &device);
-            info!("Dispatching worker thread");
-            trace!("Current strong count: {}", Arc::strong_count(state));
-            *inner = AttachedDeviceState::Processing;
-
-
-            // Dispatch a thread to handle this device, and update the state when it's done.
-            let inner_state = Arc::clone(state);
-            thread::spawn(move || {
-                info!("Starting worker thread");
-                // Urgh, does this mean I also need to wrap the devices in mutexes..
-                // work(device);
-                info!("Work finished, Marking device as complete");
-                *inner_state.lock().expect("Setting complete lock") = AttachedDeviceState::Complete;
-            });
-        }
-    }
-}
-
+// TODO(richo) lift this into archiver::cli? Or too magic?
 lazy_static! {
     static ref ctx: Ctx = {
         let cfg = config::Config::from_file("archiver.toml").expect("Couldn't create config");
@@ -108,19 +42,19 @@ lazy_static! {
 
 fn main() {
     archiver::cli::run(|| {
-        trace!("Creating device state");
-        let mut state: DeviceState = Default::default();
+        info!("Fetching configured devices");
+        let devices = ctx.cfg.configured_devices();
 
-        poll_devices(&mut state)?;
-        spawn_worker_threads(&mut state,
-                             |_device| thread::sleep(time::Duration::from_secs(10)));
-
-        // The main polling loop
-        loop {
-            info!("State: {:?}", &state);
-            poll_devices(&mut state)?;
-            thread::sleep(time::Duration::from_secs(2));
+        for device in devices {
+            // Prototyping with gopros for now
+            if let DeviceConfig::Gopro(gopro_cfg) = device {
+                dispatch_gopro_watcher(gopro_cfg);
+            } else {
+                info!("Ignoring device {:?} for now", &device);
+            }
         }
+
+        Ok(())
     })
 }
 
