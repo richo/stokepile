23ac06b2a50c8cfae9a87c305667016459b2062e
Richo Healey <richo@psych0tik.net>
2019-03-25 22:22:40 -0700

    Migrations won't work without dotenv

--- src/bin/run_migrations.rs
+++ src/bin/run_migrations.rs
@@ -1,7 +1,9 @@
+use archiver::cli::init_dotenv;
 use archiver::web::db::run_migrations;
 
 fn main() {
     archiver::cli::run(|| {
+        init_dotenv()?;
         run_migrations()?;
         Ok(())
     })
