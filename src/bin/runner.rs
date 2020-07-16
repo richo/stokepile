e49eb442c36793f34c974ded44db1f6c640c3e28
Richo Healey <richo@psych0tik.net>
2020-07-16 11:34:27 -0700

    Aggressively an unflinchingly refuse to actually fix the implementation

--- src/bin/runner.rs
+++ src/bin/runner.rs
@@ -12,6 +12,8 @@ use stokepile::mountable::Mountable;
 use stokepile::staging::Stager;
 use stokepile::storage;
 
+use std::io;
+
 fn cli_opts<'a, 'b>() -> App<'a, 'b> {
     cli::base_opts()
         .about("Performs a single run, uploading footage from all connected devices")
@@ -64,11 +66,37 @@ fn main() {
         };
 
         for device in devices {
-            let msg = format!("Finished staging: {}", device.name());
-            let num_files = device.stage_files(&stager)?;
-            if num_files > 0 {
+            let finish_msg = format!("Finished staging: {}", device.name());
+            let incomplete_msg = format!("Partially staged {}, device will need a second run", device.name());
+            let notify = |msg: String| {
                 if let Err(e) = ctx.notify(&msg) {
                     error!("Failed to send push notification: {:?}", e);
+                }
+            };
+            match device.stage_files(&stager) {
+                Ok(num_files) => {
+                    if num_files > 0 {
+                        notify(finish_msg);
+                    }
+                },
+                // TODO(richo) We probably want to just have this be an io::Error instead of
+                // faffing about with failure. As it stands, we use .context() to help with
+                // debugging which more or less implies failure, but maybe there's some
+                // middleground.
+                Err(err) => {
+                    match err.downcast::<io::Error>() {
+                        Ok(err) if err.kind() == io::ErrorKind::Interrupted => {
+                            warn!("Staging device full, continuing");
+                            notify(incomplete_msg);
+                            break
+                        },
+                        Ok(err) => {
+                            Err(err)?
+                        },
+                        Err(err) => {
+                            Err(err)?
+                        }
+                    }
                 }
             }
         }
